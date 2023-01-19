106626df4aebd64d690aa73667f5aada1e63b3c7
Fox <filipiukjarek@gmail.com>
2023-01-20 00:33:55 +0100

    c++ brainfuck interpreter - fixed while loop

--- brainfuck_interpreter/bf_interpreter.cpp
+++ brainfuck_interpreter/bf_interpreter.cpp
@@ -23,7 +23,7 @@ void bf_interpreter::decrement_val()
 
 void bf_interpreter::putchar()
 {
-    std::cout << *this->mem_ptr << std::endl;
+    std::cout << *this->mem_ptr;
 }
 
 void bf_interpreter::getchar()
@@ -33,30 +33,43 @@ void bf_interpreter::getchar()
 
 void bf_interpreter::begin_while()
 {
-    if (*this->mem_ptr !=0)
-    {
-       this->ret_addr = this->mem_ptr;
-    } else
-    {
-        this->ret_addr = NULL;
-    }
+    // if (*this->mem_ptr != 0)
+    // if (this->ret_addr_stack.top() != NULL)
+    // {
+    //    this->ret_addr = this->mem_ptr;
+    //    this->ret_addr_stack.push(this->mem_ptr);
+    // } 
+    // else
+    // {
+    //     this->ret_addr = NULL;
+    // }
     
 }
 
 void bf_interpreter::end_while()
 {
-    if (this->ret_addr != NULL)
-    {
-       this->mem_ptr = ret_addr;
-    }
+    // if (this->ret_addr != NULL)
+    // {
+    //    this->mem_ptr = ret_addr;
+    // }
     // {
     //     std::cerr << "No return address, check if \"[\" instruction was issued before";
     //     exit(0);
     // }
+    // if (this->ret_addr_stack.top() != NULL)
+    // {
+        // this->mem_ptr = this->ret_addr_stack.top();
+        // printf("%x\n", this->ret_addr_stack.top());
+        // std::cout << std::hex << this->ret_addr_stack.top() << std::endl;
+        // this->ret_addr_stack.pop();
+    // }
+    
 }
 
-int8_t bf_interpreter::parse_console_input(char bf_instruction)
+char bf_interpreter::parse_console_input(char bf_instruction)
 {
+    char ret_val = NULL;
+
     switch (bf_instruction)
     {
     case '>':
@@ -78,33 +91,69 @@ int8_t bf_interpreter::parse_console_input(char bf_instruction)
         this->getchar();
         break;
     case '[':
-        this->begin_while();
+        // this->begin_while();
+        // if (*this->mem_ptr != 0)
+        {
+            ret_val = bf_instruction;
+        }
         break;
     case ']':
-        this->end_while();
+        // this->end_while();
+        ret_val = bf_instruction;
         break;
     
     default:
         std::cerr << "\"" << *this->mem_ptr << "\" is not a valis brainfuck instruction, please check your code" << std::endl;
         break;
     }
+    return ret_val;
 }
 
 void bf_interpreter::start()
 {
     std::string input_line;
-    
-    while (std::getline(std::cin, input_line))
+    char loop_instr = NULL;    
+   while (std::getline(std::cin, input_line))
     {
         if (this->termination_str == input_line)
         {
            std::cout << "Brainfuck terminated... exiting interpreter" << std::endl;
            break;
         }
-        
-        for (size_t i = 0; i < input_line.size(); i++)
+
+        for (instr_ptr = input_line.begin(); instr_ptr != input_line.end(); instr_ptr++)
+        // for (instr_ptr = ; i < count; i++)
+        // for (size_t i = 0; i < input_line.size(); i++)
         {
-            this->parse_console_input(input_line[i]);
+            loop_instr = this->parse_console_input(*instr_ptr);
+
+            if (loop_instr == '[')
+            {
+                if(*this->mem_ptr != 0)
+                {
+                    this->while_begin_stack.push(instr_ptr);
+                }
+                else
+                {
+                    // go to one instruction after matching ]
+                    instr_ptr = this->while_end_stack.top();
+                    // instr_ptr++;
+                    this->while_end_stack.pop();   
+                }
+            } 
+            else if (loop_instr == ']')
+            {
+                if (*this->mem_ptr == 0)
+                {
+                    this->while_end_stack.push(instr_ptr);
+                }
+                
+                //  go to one before matching [ instruction
+                instr_ptr = this->while_begin_stack.top();
+                instr_ptr--;
+                this->while_begin_stack.pop();
+            }
+            // std::cout << "instruction: " << *instr_ptr << std::endl;// ", mem"            
         }
     }
     
--- brainfuck_interpreter/bf_interpreter.hpp
+++ brainfuck_interpreter/bf_interpreter.hpp
@@ -2,12 +2,16 @@
 
 #include <stdint.h>
 #include <string>
+#include <stack>
 
 class bf_interpreter
 {
 private:
-    uint8_t* mem_ptr;
-    uint8_t* ret_addr;    
+    uint8_t *mem_ptr;
+    // uint8_t* ret_addr;
+    std::string::iterator instr_ptr;
+    std::stack<std::string::iterator> while_begin_stack;
+    std::stack<std::string::iterator> while_end_stack;   
     std::string termination_str = "quit";
     uint8_t array[30000] = {0};
     void increment_ptr();
@@ -18,12 +22,14 @@ private:
     void getchar();
     void begin_while();
     void end_while();
-    int8_t parse_console_input(char bf_instruction);
+    char parse_console_input(char bf_instruction);
 
 public:
     bf_interpreter() {
-        mem_ptr = array;
-        ret_addr = NULL;
+        this->mem_ptr = array;
+        // ret_addr = NULL;
+    //    this->while_end_stack.push(NULL);
+    //    this->while_begin_stack.push(NULL) 
     }
     ~bf_interpreter() {}
 
