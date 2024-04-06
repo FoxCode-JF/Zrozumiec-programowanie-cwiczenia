0d0e24eebc39984c25fe3af0f40f67c579e47163
Fox <filipiukjarek@gmail.com>
2024-04-06 11:23:49 +0200

    bignum init

---
+++ chapter_4/INT:bignum/bignum.c
@@ -0,0 +1,33 @@
+
+typedef struct
+{
+    int32_t *num;
+    size_t numSize;
+} big_int_t;
+
+bignum_show()
+{
+}
+
+/**
+ * @brief Turns text representation of a number
+ *  into big numbers type (big_int_t)
+ *
+ * @param number string representation of a bignum
+ * @return big_int_t
+ */
+big_int_t bignum_strToBig(char *number)
+{
+}
+
+char *bignum_bigToStr(big_int_t)
+{
+}
+
+big_int_t bignum_add(big_int_t a, big_int_t b)
+{
+}
+
+big_int_t bignum_multiply(big_int_t a, big_int_t b)
+{
+}
